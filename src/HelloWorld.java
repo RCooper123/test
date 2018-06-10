1225133afd5b5e948816c9fff98a1ec3ee19f717
John Cooper <catweazle9@outlook.com>
2018-06-10 19:43:22 +0100

    Adding a comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+        // Adding a comment!
 		System.out.println("Goodbye world!");
 	}
 }
